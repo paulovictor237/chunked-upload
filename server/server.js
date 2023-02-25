8c1ddbba0a86b5936f7c3672fc5afe9c1a856255
peve <paulovictor237@gmail.com>
2023-02-25 15:55:58 -0300

    refactor sever.js

--- server/server.js
+++ server/server.js
@@ -5,31 +5,33 @@ import fs from "fs";
 import md5 from "md5";
 
 const app = express();
-app.use(bodyParser.raw({type:'application/octet-stream', limit:'100mb'}));
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
-app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.raw({ type: "application/octet-stream", limit: "100mb" }));
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use("/uploads", express.static("uploads"));
 
-app.post('/upload', (req, res) => {
-  const {name,currentChunkIndex,totalChunks} = req.query;
-  const firstChunk = parseInt(currentChunkIndex) === 0;
-  const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) -1;
-  const ext = name.split('.').pop();
-  const data = req.body.toString().split(',')[1];
-  const buffer = new Buffer(data, 'base64');
-  const tmpFilename = 'tmp_' + md5(name + req.ip) + '.' + ext;
-  if (firstChunk && fs.existsSync('./uploads/'+tmpFilename)) {
-    fs.unlinkSync('./uploads/'+tmpFilename);
-  }
-  fs.appendFileSync('./uploads/'+tmpFilename, buffer);
-  if (lastChunk) {
-    const finalFilename = md5(Date.now()).substr(0, 6) + '.' + ext;
-    fs.renameSync('./uploads/'+tmpFilename, './uploads/'+finalFilename);
-    res.json({finalFilename});
-  } else {
-    res.json('ok');
-  }
+app.post("/upload", (req, res) => {
+  const { name, currentChunkIndex, totalChunks } = req.query;
+  const { ip, body } = req;
+
+  const extension = name.split(".").pop();
+  const tmpFilename = "tmp_" + md5(name + ip) + "." + extension;
+
+  const isFirstChunk = +currentChunkIndex === 0;
+  const isLastChunk = +currentChunkIndex === +totalChunks - 1;
+  const fileExists = fs.existsSync("./uploads/" + tmpFilename);
+
+  if (isFirstChunk && fileExists) fs.unlinkSync("./uploads/" + tmpFilename);
+
+  const base64 = body.toString();
+  const data = base64.split(",")[1];
+  const buffer = Buffer.from(data, "base64");
+  fs.appendFileSync("./uploads/" + tmpFilename, buffer);
+
+  if (!isLastChunk) return res.json("ok");
+
+  const finalFilename = md5(Date.now()).slice(0, 6) + "." + extension;
+  fs.renameSync("./uploads/" + tmpFilename, "./uploads/" + finalFilename);
+  res.json({ finalFilename });
 });
 
 app.listen(4001);
